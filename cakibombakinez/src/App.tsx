a81519e3dc3104fca8d1b8d3d8412622a3ec65e7
pop <dusanplavsic19@gmail.com>
2024-04-20 01:15:28 +0200

    bomba

--- cakibombakinez/src/App.tsx
+++ cakibombakinez/src/App.tsx
@@ -3,14 +3,18 @@ import './App.css';
 import NodePage from './Components/NodeCard/NodePage';
 import AppNavbar from './Components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HeroSection from './Components/Hero/Hero';
+import AboutSection from './Components/AboutSection/AboutSection';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<AppNavbar/>
-				<NodePage/>
-			</header>
+			<div className="App-header wrapper ">
+				<AppNavbar />
+				<HeroSection />
+				<AboutSection />
+				<NodePage />
+			</div>
 		</div>
 	);
 }
